4f12a134f25b2b4126aaa3277a93d3b9b90dbf35
Lucie Wolf <wolfl@carleton.edu>
2023-11-08 10:11:08 -0600

    Code to run all models and save all outputs

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ from pathlib import Path
     For more infos on how to use it, go to its Github repository at: https://github.com/dotXem/GLYFE """
 
 
-def main(dataset, subject, model, params, exp, mode, log, ph, plot):
-    printd(dataset, subject, model, params, exp, mode, log, ph, plot)
+def main(dataset, subject, model, params_name, exp, mode, log, ph, plot):
+    printd(dataset, subject, model, params_name, exp, mode, log, ph, plot)
 
     # retrieve model's parameters
     model_class = locate_model(model)
-    search = locate_search(params)
-    params = locate_params(params)
+    search = locate_search(params_name)
+    params = locate_params(params_name)
     
 
     # scale variables in minutes to the benchmark sampling frequency
@@ -43,7 +43,12 @@ def main(dataset, subject, model, params, exp, mode, log, ph, plot):
     results = ResultsSubject(model, exp, ph, dataset, subject, params=params, results=raw_results)
     printd(results.compute_results())
     if plot:
-        dir = os.path.join(cs.path, "plots", model, exp, "ph-" + str(ph))
+        print('here')
+        print(f'params: {params_name}')
+        print(f'exp: {exp}')
+        print(f'ph: {ph}')
+        dir = os.path.join(cs.path, "plots", params_name, exp, "ph-" + str(ph))
+        print('past here')
         Path(dir).mkdir(parents=True, exist_ok=True)
         if mode == 'valid':
             file_path = os.path.join(dir, dataset + "_" + subject + "_valid.png")
@@ -98,7 +103,7 @@ if __name__ == "__main__":
          subject=args.subject,
          model=args.model,
          ph=args.ph,
-         params=args.params,
+         params_name=args.params,
          exp=args.exp,
          dataset=args.dataset,
          mode=args.mode,
--- runit.bash
+++ runit.bash
@@ -1,18 +1,37 @@
 #!/bin/bash
 DATASET="ohio"
-SUBJECT="559"
-MODEL="base"
-PHS=("60" "120")
-EXP="test_bash"
-PARAMS="base"
+SUBJECTS="559 563 570 575 588 591"
+PHS="30 60 120"
+EXP="NOV_3_2023"
 MODE="test"
 PLOT="1"
 
-# something like this
-#python main.py --dataset=$DATASET --mode=$MODE -- 2>&1 $DIR/output-gunk.txt
-for PH in "60" "120"; do 
-echo $PH
-DIR="outputs/$MODEL/$EXP/ph-$PH"
-mkdir -p $DIR
-python main.py --dataset=$DATASET --subject=$SUBJECT --model=$MODEL --params=$PARAMS --ph=$PH --exp=$EXP --mode=$MODE --plot=$PLOT > $DIR/$DATASET$()_$SUBJECT.txt
+declare -A PARAMS_MODELS
+PARAMS_MODELS["ar"]="arimax"
+PARAMS_MODELS["arx"]="arimax"
+PARAMS_MODELS["base"]="base"
+PARAMS_MODELS["elm"]="elm"
+PARAMS_MODELS["ffnn"]="ffnn"
+PARAMS_MODELS["gp"]="gp"
+PARAMS_MODELS["lstm"]="lstm"
+PARAMS_MODELS["poly"]="poly"
+PARAMS_MODELS["svr"]="svr"
+
+for SUBJECT in $SUBJECTS; do 
+    echo $SUBJECT
+    
+    for PH in $PHS; do 
+        echo $PH
+
+        for PARAM in ${!PARAMS_MODELS[@]}; do
+            
+            MODEL=${PARAMS_MODELS[$PARAM]}
+            echo $PARAM
+            echo $MODEL
+
+            DIR="outputs/$PARAM/$EXP/ph-$PH"
+            mkdir -p $DIR
+            python main.py --dataset=$DATASET --subject=$SUBJECT --model=$MODEL --params=$PARAM --ph=$PH --exp=$EXP --mode=$MODE --plot=$PLOT > $DIR/$DATASET$()_$SUBJECT.txt
+        done
+    done
 done
