4b2965e632e49b86aeba2abf4e9513d0e682d21c
Erin Watson <watson.e.and@gmail.com>
2024-04-17 12:51:22 -0500

    Some of the tidepool results for PH 30 on one patient

--- runit.bash
+++ runit.bash
@@ -1,21 +1,21 @@
 #!/bin/bash
-DATASET="ohio"
-SUBJECTS="559 563 570 575 588 591"
+DATASET="tidepool"
+SUBJECTS="0"
 PHS="30 60 120"
-EXP="DEC_8_2023"
+EXP="FIRST_TIDEPOOL"
 MODE="test"
 PLOT="1"
 
 declare -A PARAMS_MODELS
-# PARAMS_MODELS["ar"]="arimax"
-# PARAMS_MODELS["arx"]="arimax"
-# PARAMS_MODELS["base"]="base"
-# PARAMS_MODELS["elm"]="elm"
-# PARAMS_MODELS["ffnn"]="ffnn"
+PARAMS_MODELS["ar"]="arimax"
+PARAMS_MODELS["arx"]="arimax"
+PARAMS_MODELS["base"]="base"
+PARAMS_MODELS["elm"]="elm"
+PARAMS_MODELS["ffnn"]="ffnn"
 PARAMS_MODELS["gp"]="gp"
-# PARAMS_MODELS["lstm"]="lstm"
-# PARAMS_MODELS["poly"]="poly"
-# PARAMS_MODELS["svr"]="svr"
+PARAMS_MODELS["lstm"]="lstm"
+PARAMS_MODELS["poly"]="poly"
+PARAMS_MODELS["svr"]="svr"
 
 for SUBJECT in $SUBJECTS; do 
     echo $SUBJECT
