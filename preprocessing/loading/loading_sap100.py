40c49995d0d210c60ebfbd1ca30d93b7b403b4bd
Erin Watson <watson.e.and@gmail.com>
2024-04-29 10:21:10 -0500

    Preparing codebase to use SAP100 data (mostly renaming things from when we did this the first time)

--- preprocessing/loading/loading_sap100.py
+++ preprocessing/loading/loading_sap100.py
@@ -9,31 +9,25 @@ import numpy as np
 import misc.constants as cs
 import misc.datasets
 
-def load_tidepool(dataset, subject):
+def load_sap100(dataset, subject):
     """
-    Load a Tidepool file into a dataframe
+    Load a Tidepool SAP100 file into a dataframe
+    This same function might apply to other Tidepool datasets like PA50 and HCL150, I don't know.
     :param dataset: name of dataset
     :param subject: name of subject
     :param day_len: length of day scaled to sampling frequency
     :return: dataframe
     """
-    df = pd.read_csv(join(cs.path, "data", dataset, subject + ".csv"), low_memory=False)
+    df = pd.read_csv(join(cs.path, "data", dataset, "short-names", subject + ".csv"), low_memory=False)
     # pull in just the data needed to run the GLYFE metric (no extras!)
-    df_glucose_carb_bolus = df[["cDateTime", "mg_dL", "grams", "normal", "extendedAmount"]].copy()
+    df_glucose_carb_bolus = df[["cDateTime", "blood_glucose_level", "carbs", "normal", "extendedAmount"]].copy()
     # aggregate bolus data and rename columns
     df_glucose_carb_bolus["insulin"] = df_glucose_carb_bolus["normal"] + df_glucose_carb_bolus["extendedAmount"]
     df_glucose_carb_bolus = df_glucose_carb_bolus.drop(columns=["normal", "extendedAmount"])
-    df_glucose_carb_bolus.rename(columns={"cDateTime": "datetime", "mg_dL": "glucose", "grams": "CHO"}, inplace=True)
+    df_glucose_carb_bolus.rename(columns={"cDateTime": "datetime", "blood_glucose_level": "glucose", "carbs": "CHO"}, inplace=True)
     # convert datetime to a datetime object
     df_glucose_carb_bolus["datetime"] = pd.to_datetime(df_glucose_carb_bolus["datetime"])
 
     print(df_glucose_carb_bolus.head())
     return df_glucose_carb_bolus
   
-
-    # df.datetime = (df.datetime % day_len).astype("float64")
-    # start_day = datetime.datetime.strptime("2020-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
-    # day_len_ds = day_len * cs.freq / misc.datasets.datasets[dataset]["glucose_freq"]
-    # end_day = start_day + datetime.timedelta(days=np.float64(len(df) // day_len_ds)) - datetime.timedelta(minutes=1)
-    # df.datetime = pd.period_range(start_day, end_day, freq='5min').to_timestamp()
-    # return df
